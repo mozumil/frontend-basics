904f4869ab160aaa07386f169fb628daa6adf591
mozumil <184168538+mozumil@users.noreply.github.com>
2025-10-05 00:28:46 +0500

    modified:   JavaScript/1.employee-details/employee_details.html         modified:   JavaScript/1.employee-details/employee_details.js

--- JavaScript/1.employee-details/employee_details.js
+++ JavaScript/1.employee-details/employee_details.js
@@ -5,3 +5,16 @@ const employees = [
     
     //... More employee records can be added here
 ];
+
+// Function to display all employees
+function displayEmployees() {
+const totalEmployees = employees
+.map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`)
+.join('');
+document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+
+function calculateTotalSalaries() {
+    const totalSaleries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total Salaries: &&{totalSalaries}`);
+}
