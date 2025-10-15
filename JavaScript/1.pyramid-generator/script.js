bda385d7b8435c888f596f3d8bb40bb8ce25de82
mozumil <184168538+mozumil@users.noreply.github.com>
2025-10-15 20:27:48 +0500

    modified:   JavaScript/1.pyramid-generator/script.js

--- JavaScript/1.pyramid-generator/script.js
+++ JavaScript/1.pyramid-generator/script.js
@@ -2,11 +2,13 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
-
+function padRow(name) {
+  return name;
 }
-const call = padRow();
 
+
+const call = padRow("CamperChan");
+console.log(call);
 
 
 for (let i = 0; i < count; i = i + 1) {
