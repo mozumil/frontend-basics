bd301bdc180413612512eefe9a563f8d5a52efde
mozumil <184168538+mozumil@users.noreply.github.com>
2025-10-17 19:53:50 +0500

    modified:   JavaScript/1.pyramid-generator/script.js

--- JavaScript/1.pyramid-generator/script.js
+++ JavaScript/1.pyramid-generator/script.js
@@ -3,11 +3,12 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+
 }
 
+
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+  rows.push(character.repeat(i + 1))
 }
 
 let result = ""
